fd7407487e9ba291291a0d2de61f16ce8e092e83
Thembapa <thembapa@gmail.com>
2020-03-10 11:36:43 +0200

    MenuBar
    Added menubar for testing

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__, static_url_path='')
 
 ## Global variebles
 GID = "67980471209-beho86sujost0htubv5iti646qeal2ab.apps.googleusercontent.com"
-
+menubuttons = [{'ButtonName': 'HOME', 'ButtonURL': '/0'}, {'ButtonName': 'INDEX', 'ButtonURL': '/all'},
+               {'ButtonName': 'CONTROL', 'ButtonURL': '/control'}, {'ButtonName': 'REGISTER', 'ButtonURL': '/register'}]
 
 @app.route('/UserNames/<username>')
 def getallusernames(username = None):
@@ -24,7 +25,11 @@ def getallusernames(username = None):
 
 @app.route('/')
 def index():
-    return render_template('index.html', GID = GID)
+    return render_template('index.html',)
+
+@app.route('/login')
+def Login():
+    return render_template('LogIn.html', GID = GID)
 
 
 if __name__ == "__main__":
