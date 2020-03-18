21e96d597f97bdb82849c046359a08d8acba9325
Thembapa <thembapa@gmail.com>
2020-03-18 17:14:28 +0200

    add master page
    masterpage has been added to allow inheritance. this will make it easy to control changes on the code going foward

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session
 import json
-
+from passlib.hash import sha256_crypt
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 app.secret_key = "kasivoretest"
@@ -8,8 +8,7 @@ app.secret_key = "kasivoretest"
 
 ## Global variebles
 GID = "67980471209-beho86sujost0htubv5iti646qeal2ab.apps.googleusercontent.com"
-menubuttons = [{'ButtonName': 'HOME', 'ButtonURL': '/0'}, {'ButtonName': 'INDEX', 'ButtonURL': '/all'},
-               {'ButtonName': 'CONTROL', 'ButtonURL': '/control'}, {'ButtonName': 'REGISTER', 'ButtonURL': '/register'}]
+menubuttons = {'Home': '/', 'About':'/About', 'Legal':'/Legal', 'Pay': '/Pay', 'Contact': '/Contact', 'Help': 'Help'}
 
 
 ### Application Fuctions###
@@ -19,6 +18,7 @@ def changetolowercase(Distionary):
         outdict[k.lower()] = Distionary[k]
 
     return outdict
+
 
 @app.route('/UserNames/<username>')
 def getallusernames(username = None):
@@ -38,44 +38,55 @@ def getallusernames(username = None):
 ##HTML pages redirect
 @app.route('/Help')
 def Help():
-    return render_template('Help.html',)
+    return render_template('Help.html', menubuttons = menubuttons)
 
 
 @app.route('/About')
 def About():
-    return render_template('About.html',)
+    return render_template('About.html', menubuttons = menubuttons)
 
 
 @app.route('/Contact')
 def Contact():
-    return render_template('Contact.html',)
+    return render_template('Contact.html', menubuttons = menubuttons)
 
 
 @app.route('/Legal')
 def Legal():
 
-    return render_template('Legal.html',)
+    return render_template('Legal.html', menubuttons = menubuttons)
+
 
 @app.route('/Pay')
 def Pay():
-    return render_template('Pay.html',)
+    return render_template('Pay.html', menubuttons = menubuttons)
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html',)
+    test = 'themba'
+    password = sha256_crypt.encrypt(test)
+    password2 = sha256_crypt.encrypt(test)
+    if password == password2:
+        print('corret')
+    else:
+        print('no match')
+    print(sha256_crypt.verify(password2, password))
+    print(password)
+    return render_template('index.html', menubuttons = menubuttons)
 
-@app.route('/login')
+
+@app.route('/login', methods=["GET", "POST"])
 def Login():
+    SignIn = ''
     if 'CurrentUser' in session:
-        s = session['CurrentUser']
-        print('you are signed in as: ', s)
-    return render_template('LogIn.html', GID = GID)
+        CurrentUser = session['CurrentUser']
+    return render_template('LogIn.html', GID = GID, menubuttons = menubuttons)
 
 
 if __name__ == "__main__":
 
-    app.run(host='192.168.0.194', port=8080)
-    #app.run()
+    #app.run(host='192.168.0.194', port=8080)
+    app.run()
 
